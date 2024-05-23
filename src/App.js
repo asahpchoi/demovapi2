e25236d11539a45b011af73aec349f1307cb99c0
asahpchoi <asa.choi@gmail.com>
2024-05-24 01:12:43 +0800

    adding vapi in

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+ 
+import TextField from "@mui/material/TextField";
+import Fab from '@mui/material/Fab';
 
-function App() {
+import { useState } from "react";
+import Stack from "@mui/material/Stack";
+import { call } from "./util.js"; 
+import CallIcon from '@mui/icons-material/Call';
+import StopIcon from '@mui/icons-material/Stop';
+export default function App() {
+  //const private_key = "9175acca-10da-411e-adb4-60d9fb4b0851";
+
+  const [welcomeMessage, setWelcomeMessage] = useState("how are you?");
+  const [prompt, setPrompt] = useState(`you are a professional agent`);
+  const [isCalling, setIsCalling] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card fullWidth>
+        <CardMedia />
+        <CardContent>
+          <Stack spacing={2}>
+            <TextField
+              fullWidth
+              label="Welcome Message"
+              value={welcomeMessage}
+              onChange={(e) => {
+                setWelcomeMessage(e.target.value);
+              }}
+            />
+            <br />
+            <TextField
+              multiline
+              rows="5"
+              label="Prompt / Instruction"
+              fullWidth
+              value={prompt}
+              onChange={(e) => {
+                setPrompt(e.target.value);
+              }}
+            />
+          </Stack>
+        </CardContent>
+        <CardActions  >
+          <Stack direction="row" spacing={2}>
+            <Fab style={{"backgroundColor": "green"}}
+              onClick={() => {
+                call(welcomeMessage, prompt);
+                setIsCalling(true);
+              }}
+              disabled={isCalling}
+            >
+              <CallIcon />
+            </Fab>
+            <Fab  disabled={!isCalling}
+style={{"backgroundColor": "red"}}
+              onClick={() => {
+
+                setIsCalling(false);
+                window.location.reload();
+              }}
+            >
+              <StopIcon />
+            </Fab>
+   
+          </Stack>
+
+
+
+        </CardActions>
+      </Card>
     </div>
   );
 }
-
-export default App;
---
+++ src/util.js
@@ -0,0 +1,24 @@
+import Vapi from "@vapi-ai/web";
+let vapi = new Vapi("94631ff3-214f-497f-b8f2-fca0ed896306");
+export function call(welcomeMessage, prompt) {
+  const assistantOverrides = {
+    transcriber: {
+      provider: "deepgram",
+      model: "nova-2",
+      language: "en-US",
+    },
+    recordingEnabled: false,
+    firstMessage: welcomeMessage,
+    model: {
+      provider: "openai",
+      model: "gpt-4o",
+      messages: [
+        {
+          role: "system",
+          content: prompt,
+        },
+      ],
+    },
+  };
+  vapi.start("cb6e3612-eb7e-463e-9041-ad17021669d7", assistantOverrides);
+}
