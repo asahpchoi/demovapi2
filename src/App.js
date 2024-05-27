4aa6044fba86d72101e84ddfd4d27f08434b0a87
asahpchoi <asa.choi@gmail.com>
2024-05-27 16:48:10 +0800

    X

--- src/App.js
+++ src/App.js
@@ -148,7 +148,7 @@ export default function App() {
                 }
               }}
             />
-            <input type="file" onChange={async (evt) => {
+            <input type="file" accept="image/*" onChange={async (evt) => {
               const convertBase64 = (file) => {
                 return new Promise((resolve, reject) => {
                   const fileReader = new FileReader();
