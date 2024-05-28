a4f6483781894d54b456a06bed5df82129bfaa21
Asa Choi <asa.choi@gmail.com>
2024-05-28 05:29:37 +0000

    X

--- src/ui/TextUI.js
+++ src/ui/TextUI.js
@@ -3,7 +3,10 @@ import {
     Stack,
     TextField,
     Typography,
+    Dialog
 } from "@mui/material";
+import { useState } from "react";
+
 import {
 
     Stop as StopIcon,
@@ -15,77 +18,84 @@ import Markdown from 'react-markdown';
 export const TextUI = ({ args }) => {
 
     const { setUserPrompt, setAnswer, callLLM, prompt, userPrompt, image, history, setHistory, setIsTexting, setImage, answer } = args
+    const [open, setOpen] = useState(false);
+
     return <div className="fullscreen">
         <Stack justifyContent="center" className="overlay">
-            <TextField
-                label="Ask a question"
-                fullWidth
-                value={userPrompt}
-                onChange={(e) => setUserPrompt(e.target.value)}
-                onKeyDown={async (e) => {
-                    if (e.keyCode === 13) {
-                        var answerPart = '';
-                        setUserPrompt("");
-                        setAnswer(".....")
-                        const answer = await callLLM(prompt, userPrompt, image,
-                            data => {
-                                if (data) {
-                                    answerPart += data;
-                                    setAnswer(answerPart)
-                                }
-                            }, history);
-                        const newHistory = history;
-                        history.push(
-                            {
-                                role: "user",
+            <Stack className="content">
+                <TextField
+                    label="Ask a question"
+                    fullWidth
+                    value={userPrompt}
+                    onChange={(e) => setUserPrompt(e.target.value)}
+                    onKeyDown={async (e) => {
+                        if (e.keyCode === 13) {
+                            var answerPart = '';
+                            setUserPrompt("");
+                            setAnswer(".....")
+                            const answer = await callLLM(prompt, userPrompt, image,
+                                data => {
+                                    if (data) {
+                                        answerPart += data;
+                                        setAnswer(answerPart)
+                                    }
+                                }, history);
+                            const newHistory = history;
+                            history.push(
+                                {
+                                    role: "user",
+                                    content:
+                                        [
+                                            {
+                                                type: "text",
+                                                text: userPrompt
+                                            }
+                                        ]
+
+                                })
+                            history.push({
+                                role: "assistant",
                                 content:
                                     [
                                         {
                                             type: "text",
-                                            text: userPrompt
+                                            text: answerPart
                                         }
                                     ]
 
                             })
-                        history.push({
-                            role: "assistant",
-                            content:
-                                [
-                                    {
-                                        type: "text",
-                                        text: answerPart
-                                    }
-                                ]
-
+                            setHistory([...history]);
+                            //console.log(history)
+                        }
+                    }}
+                />
+                <input type="file" id="imageCapture" accept="image/*" capture="environment" style={{ display: 'none' }} onChange={async (evt) => {
+                    const convertBase64 = (file) => {
+                        return new Promise((resolve, reject) => {
+                            const fileReader = new FileReader();
+                            fileReader.readAsDataURL(file)
+                            fileReader.onload = () => {
+                                resolve(fileReader.result);
+                            }
+                            fileReader.onerror = (error) => {
+                                reject(error);
+                            }
                         })
-                        setHistory([...history]);
-                        //console.log(history)
                     }
-                }}
-            />
-            <input type="file" id="imageCapture" accept="image/*" capture="environment" style={{ display: 'none' }} onChange={async (evt) => {
-                const convertBase64 = (file) => {
-                    return new Promise((resolve, reject) => {
-                        const fileReader = new FileReader();
-                        fileReader.readAsDataURL(file)
-                        fileReader.onload = () => {
-                            resolve(fileReader.result);
-                        }
-                        fileReader.onerror = (error) => {
-                            reject(error);
-                        }
-                    })
-                }
-                const file = evt.target.files[0];
-                const base64 = await convertBase64(file);
-                setImage(base64);
-            }} />
-            <div>             
-                <Markdown>{answer}</Markdown>
-            </div>
-            <div>
-                <img style={{ width: '10vh' }} src={image} />
-            </div>
+                    const file = evt.target.files[0];
+                    const base64 = await convertBase64(file);
+                    setImage(base64);
+                }} />
+                <div>
+                    <Markdown>{answer}</Markdown>
+                </div>
+                <div>
+                    <img style={{ width: '10vw' }} src={image} onClick={() => setOpen(true)} />
+                </div>
+                <Dialog open={open} style={{ width: '90vw' }}>
+                    <img style={{ width: '90vw' }} src={image} onClick={() => setOpen(false)} />
+                </Dialog>
+            </Stack>
             <Stack direction="row" justifyContent="center" spacing={2} className="footer">
                 <Fab onClick={() => setIsTexting(false)}><StopIcon /></Fab>
                 <Fab onClick={() => {
