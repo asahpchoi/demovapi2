c55c5144d145d11bce09c9394c0d6b27f83a1c90
asahpchoi <asa.choi@gmail.com>
2024-06-01 01:27:36 +0800

    X

--- src/ui/Login.js
+++ src/ui/Login.js
@@ -14,20 +14,20 @@ import listen from "../images/listen.svg";
 export const Login = ({ args }) => {
     const [inputName, setInputName] = useState("");
 
-    return <Stack className="overlay" >
-        <img alt="BG" loading="lazy" src={bg} className="absolute bottom-0" />
+    return <Stack className="overlay flex justify-center" >
+        <img alt="BG" loading="lazy" src={bg} className="absolute z-0 bottom-0" />
         <div className="absolute top-0 left-0 flex align-middle   ">
             <img src={logo} className="w-40" />
             <h2 className="p-10 font-bold">FWD Gen AI build your bot</h2>
         </div>
-        <div className="flex flex-col text-center mt-40">
-            <img src={listen} className="h-40"/>
-            <div className="text-3xl w-auto font-bold">Hello, Nice to have you here.</div>
-            <div className="text-5xl w-auto font-bold py-6">What is your name?</div>
-            <div className="flex" >
+        <div className="flex flex-col text-center">
+            <img src={listen} className="h-48" />
+            <div className="text-3xl w-auto font-bold z-1">Hello, Nice to have you here.</div>
+            <div className="text-5xl w-auto font-bold py-6 z-1">What is your name?</div>
+            <div className="flex z-1" >
                 <div className="flex-grow"></div>
-                <TextField className="w200" 
-                    label="Your Name"
+                <TextField className="w300 bg-white"
+                    label="Name"
                     value={inputName}
                     onChange={(e) => setInputName(e.target.value)}
                 />
