6d54295e31dd88414c2b0539aff4b0acf09e0bac
asahpchoi <asa.choi@gmail.com>
2024-06-01 17:35:22 +0800

    X

--- src/ui/TextUI.js
+++ src/ui/TextUI.js
@@ -14,7 +14,53 @@ export const TextUI = ({ args }) => {
     const { setUserPrompt, setAnswer, callLLM, prompt, userPrompt, image, setImage, history, setHistory, answer, rag, setDisplayMode, model } = args
 
     function buildMessages() {
+    }
 
+    async function handleSubmit() {
+        var answerPart = '';
+        history.push(
+            {
+                role: "user",
+                content:
+                    [
+                        {
+                            type: "text",
+                            text: userPrompt
+                        }
+                    ]
+            })
+        setUserPrompt("");
+        setAnswer(".....")
+
+        const answer = await callLLM(prompt, userPrompt, image,
+            (data, fin) => {
+                if (data) {
+
+                    answerPart += data;
+                    setAnswer(answerPart)
+                }
+                if (fin) {
+
+                    history.push({
+                        role: "assistant",
+                        content:
+                            [
+                                {
+                                    type: "text",
+                                    text: answerPart
+                                }
+                            ],
+                        model: model
+
+                    })
+                    setHistory([...history]);
+                    setAnswer("");
+                }
+                const chatbox = document.getElementById("chatbox");
+                chatbox.scrollTop = 10000;
+
+            }, history, rag, model);
+        const newHistory = history;
     }
 
     function convertMessages(ms) {
@@ -31,8 +77,7 @@ export const TextUI = ({ args }) => {
     return <Stack className="halfpage">
 
         <Stack className="z1 flex mb-5" direction="row">
-            <div style={{ width: image ? '70%' : '100%', height: '65vh', overflow: 'auto', textAlign: "left" }}>
-
+            <div id="chatbox" style={{ width: image ? '70%' : '100%', height: '65vh', overflow: 'auto', textAlign: "left" }}>
                 <Stack className="p-2">
                     {history.map(h => {
                         const data = h.content[0].text
@@ -45,69 +90,29 @@ export const TextUI = ({ args }) => {
                         <LLMIcon name={model} ></LLMIcon>
                         <Markdown className="pl-2">{answer}</Markdown>
                     </Stack>}
-
                 </Stack>
-
             </div>
             {image && <div>
                 <ImageControl image={image} setImage={setImage} />
             </div>}
 
         </Stack>
-        <TextField
-            label="Ask a question"
-            fullWidth
-            value={userPrompt}
-            onChange={(e) => setUserPrompt(e.target.value)}
-            className="z1"
-            onKeyDown={async (e) => {
-                if (e.keyCode === 13) {
-                    var answerPart = '';
-                    history.push(
-                        {
-                            role: "user",
-                            content:
-                                [
-                                    {
-                                        type: "text",
-                                        text: userPrompt
-                                    }
-                                ]
-                        })
-                    setUserPrompt("");
-                    setAnswer(".....")
+        <Stack direction="row">
+            <TextField
+                label="Ask a question"
+                fullWidth
+                value={userPrompt}
+                onChange={(e) => setUserPrompt(e.target.value)}
+                className="z1"
+                onKeyDown={async (e) => {
+                    if (e.keyCode === 13) {
+                        handleSubmit();
+                    }
+                }}
+            />
+            <Button onClick={handleSubmit}>Ask</Button>
 
-                    const answer = await callLLM(prompt, userPrompt, image,
-                        (data, fin) => {
-                            if (data) {
+        </Stack>
 
-                                answerPart += data;
-                                setAnswer(answerPart)
-                            }
-                            if (fin) {
-
-                                history.push({
-                                    role: "assistant",
-                                    content:
-                                        [
-                                            {
-                                                type: "text",
-                                                text: answerPart
-                                            }
-                                        ],
-                                    model: model
-
-                                })
-                                setHistory([...history]);
-                                setAnswer("");
-                            }
-
-                        }, history, rag, model);
-                    const newHistory = history;
-
-
-                }
-            }}
-        />
     </Stack>
 }
