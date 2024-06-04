7570d1e751fea84443f17c9e3991acc19319d221
Matthew Lau <matt3w.lau@gmail.com>
2024-06-04 23:00:40 +0800

    fix: align with figma

--- src/ui/MainPrompt.js
+++ src/ui/MainPrompt.js
@@ -1,7 +1,4 @@
 import * as React from "react";
-import {
-  FilledInput, Button
-} from "@mui/material";
 import selected from "../images/selected.svg";
 import unselected from "../images/unselected.svg";
 import openai from "../images/openai.png";
@@ -12,29 +9,21 @@ import documentIcon from "../images/document.svg"
 import quote from "../images/quote.svg"
 import untoggled from "../images/untoggled.svg"
 import toggled from "../images/toggled.svg"
+import email from "../images/email.svg"
 
 const icons = {
   openai: openai,
   mistral: mistral,
   minimax: minimax,
 }
-/*
-const FileItem = ({ src, text, alt }) => (
-    <div className="flex gap-5 justify-between px-2 py-2 rounded bg-stone-50">
-        <div className="flex gap-1">
-            <img loading="lazy" src={src} alt={alt} className="shrink-0 w-5 aspect-square" />
-            <div className="my-auto">{text}</div>
-        </div>
-        <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/d89bee0d53fee804ad87b2cccf2b6d70465b44bc4fb312abf57535a233c953b4?apiKey=aef2252b7f4e44588501764630aaa53c&" alt="Delete icon" className="shrink-0 my-auto w-4 aspect-square" />
-    </div>
-);
-*/
-export const MainPrompt = ({ setDisplayMode, prompt, setPrompt, model, setModel, models, onOpenBot }) => {
-  // const enableButton = prompt.length > 0;
+
+export function MainPrompt({ setDisplayMode, prompt, setPrompt, model, setModel, models, onOpenBot }) {
   const [isRag, setIsRag] = React.useState(false);
+  const [text, setText] = React.useState("");
   const [isToggled, setIsToggled] = React.useState(false);
   const [selectedModel, setSelectedModel] = React.useState(null);
-  const enableButton = true;
+  const enableButton = !!text && text.length > 0;
+
   const buttonStyle = {
     alignItems: "center",
     width: "147px",
@@ -54,22 +43,11 @@ export const MainPrompt = ({ setDisplayMode, prompt, setPrompt, model, setModel,
     display: "flex",
     flex: "1",
     resize: 'none',
-  }
-
-  const radioStyle = {
-    width: "24px",
-    height: "24px",
-    borderRadius: "1000px",
-    borderColor: "#E87722",
-    borderWidth: "2px",
-  }
-
-  const selectedRadioStyle = {
-    backgroundColor: "#E87722",
+    fontSize: "14px",
   }
 
   return (
-    <div className="flex flex-col px-9 pb-9">
+    <div className="flex flex-col px-9 pb-9 w-1/2">
       <div className="text-left text-2xl font-bold text-neutral-800">
         Set up your agent bot:
       </div>
@@ -78,9 +56,10 @@ export const MainPrompt = ({ setDisplayMode, prompt, setPrompt, model, setModel,
       </div>
       <div className="flex flex-col h-[400px] justify-between p-2 mt-4 bg-white rounded-lg border-2 border-orange-500 border-solid max-md:max-w-full">
         <div className="flex flex-row flex-1 gap-2 max-md:flex-col max-md:gap-0">
-          <textarea style={inputStyle} multiple />
+          <textarea name="botPrompt" style={inputStyle} onChange={e => setText(e.target.value)} />
           <button
             style={buttonStyle}
+            disabled={enableButton}
             onClick={async () => {
             }}
           >
@@ -96,7 +75,7 @@ export const MainPrompt = ({ setDisplayMode, prompt, setPrompt, model, setModel,
             className="w-6 aspect-square max-md:mt-10"
           />
           <div className="flex gap-3 py-3 mt-3 text-sm rounded-lg max-md:flex-wrap">
-            <div className="flex gap-2 self-start px-3 font-bold rounded text-neutral-800">
+            <div className="flex gap-2 self-start px-1 font-bold rounded text-neutral-800">
               <img
                 loading="lazy"
                 src="https://cdn.builder.io/api/v1/image/assets/TEMP/18374eb6dcc8a84497e998d43bbc955e7bef76181e4ff3251b797e8138135010?"
@@ -109,7 +88,6 @@ export const MainPrompt = ({ setDisplayMode, prompt, setPrompt, model, setModel,
                 onClick={() => setSelectedModel("openai")}
                 className="flex gap-2 justify-center py-1 pr-3 pl-1 bg-white border-0 border-orange-500 border-solid rounded-[100px]">
                 <img
-                  loading="lazy"
                   src={selectedModel === "openai" ? selected : unselected}
                   className="shrink-0 w-5 aspect-square"
                 />
@@ -122,7 +100,6 @@ export const MainPrompt = ({ setDisplayMode, prompt, setPrompt, model, setModel,
                 onClick={() => setSelectedModel("minimax")}
                 className="flex gap-2 justify-center py-1 pr-3 pl-1 whitespace-nowrap bg-white border-0 border-orange-500 border-solid rounded-[100px]">
                 <img
-                  loading="lazy"
                   src={selectedModel === "minimax" ? selected : unselected}
                   className="shrink-0 w-5 aspect-square"
                 />
@@ -135,7 +112,6 @@ export const MainPrompt = ({ setDisplayMode, prompt, setPrompt, model, setModel,
                 onClick={() => setSelectedModel("mistral")}
                 className="flex gap-2 justify-center py-1 pr-3 pl-1 whitespace-nowrap bg-white border-0 border-orange-500 border-solid rounded-[100px]">
                 <img
-                  loading="lazy"
                   src={selectedModel === "mistral" ? selected : unselected}
                   className="shrink-0 w-5 aspect-square"
                 />
@@ -177,39 +153,33 @@ export const MainPrompt = ({ setDisplayMode, prompt, setPrompt, model, setModel,
           <div className="flex gap-3 mt-5 self-stretch text-xs font-bold text-neutral-800 max-md:flex-wrap">
             <div className="flex gap-1 justify-center p-2 bg-white rounded">
               <img
-                loading="lazy"
                 src={documentIcon}
                 className="shrink-0 w-5 aspect-square"
               />
               <div className="flex-1 my-auto underline">Set for Life</div>
               <img
-                loading="lazy"
                 src="https://cdn.builder.io/api/v1/image/assets/TEMP/20edc38edb55de419d162ad4a214d71d710f0594cc5ca107db3c28d5ef15d759?"
                 className="shrink-0 w-5 aspect-square"
               />
             </div>
             <div className="flex gap-1 justify-center p-2 bg-white rounded">
               <img
-                loading="lazy"
                 src={documentIcon}
                 className="shrink-0 w-5 aspect-square"
               />
               <div className="flex-1 my-auto underline">Health investment linked</div>
               <img
-                loading="lazy"
                 src="https://cdn.builder.io/api/v1/image/assets/TEMP/20edc38edb55de419d162ad4a214d71d710f0594cc5ca107db3c28d5ef15d759?"
                 className="shrink-0 w-5 aspect-square"
               />
             </div>
             <div className="flex gap-1 justify-center p-2 bg-white rounded">
               <img
-                loading="lazy"
                 src={documentIcon}
                 className="shrink-0 w-5 aspect-square"
               />
               <div className="flex-1 my-auto underline">Claim process</div>
               <img
-                loading="lazy"
                 src="https://cdn.builder.io/api/v1/image/assets/TEMP/20edc38edb55de419d162ad4a214d71d710f0594cc5ca107db3c28d5ef15d759?"
                 className="shrink-0 w-5 aspect-square"
               />
@@ -218,95 +188,25 @@ export const MainPrompt = ({ setDisplayMode, prompt, setPrompt, model, setModel,
         )}
       </div>
       <div className="flex gap-2 items-center pr-3 pl-4 mt-4 rounded max-md:flex-wrap max-md:pr-5 max-md:max-w-full">
+        <img
+          src={email}
+        />
         <div className="self-stretch my-auto text-sm font-bold text-neutral-800">
           Agentic AI bot (email agent)
         </div>
         <div onClick={() => setIsToggled(prev => !prev)}>
           {isToggled && (
             <img
-              loading="lazy"
               src={toggled}
               className="shrink-0 self-stretch aspect-[0.97] w-[29px]"
             />)}
           {!isToggled && (
             <img
-              loading="lazy"
               src={untoggled}
               className="shrink-0 self-stretch aspect-[0.97] w-[29px]"
             />)}
         </div>
       </div>
-    </div >
-  );
-  return (
-    <div className="flex flex-col self-stretch  bg-fwd-50 pl-10 pr-10">
-      <header className="flex gap-5 justify-between px-5 w-full font-bold max-md:flex-wrap max-md:max-w-full">
-        <h1 className="text-xl text-neutral-800">Enter prompt below:</h1>
-        <div className="text-base text-right text-orange-500" ><Button onClick={() => {
-          setDisplayMode("info")
-        }} variant="outlined">Show instructions</Button>
-
-        </div>
-      </header>
-      <section className="flex flex-col justify-between p-2 mt-4 w-full rounded-lg border-2 border-orange-500 border-solid max-md:max-w-full">
-        <div className="max-md:max-w-full">
-          <div className="flex gap-5 max-md:flex-col max-md:gap-0">
-            <article className="flex flex-col w-full max-md:ml-0 max-md:w-full">
-              <p className="grow pt-2.5 pb-6 text-sm leading-5 font-[450] text-neutral-800 max-md:mt-3">
-                <FilledInput value={prompt} className="w-full h-300" rows={3} multiline
-                  onChange={(e) => {
-                    setPrompt(e.target.value)
-                  }} />
-              </p>
-            </article>
-
-          </div>
-        </div>
-        <nav className="flex gap-4  max-md:flex-wrap max-md:pr-5 flex-row-reverse">
-          <Button className="m-1" onClick={() => {
-            setDisplayMode("test")
-          }} variant="outlined"> Start conversation</Button>
-          <img loading="lazy" src={uploadimage} alt="Second external link" className="shrink-0 w-6 aspect-square"
-            onClick={() => { setDisplayMode("upload") }} />
-
-        </nav>
-      </section>
-      <section className="flex gap-3 py-3 mt-6 text-sm rounded-lg max-md:flex-wrap">
-        <div className="flex gap-2 self-start px-3 font-bold rounded text-neutral-800">
-          <img loading="lazy" src={quote} alt="Select LLM icon" className="shrink-0 aspect-[0.97] w-[29px]" />
-          <span className="my-auto">Select LLM:</span>
-        </div>
-        <div className="flex flex-wrap flex-1 gap-3 content-start pr-20 font-[450] text-neutral-800">
-          {
-
-            models.map((item, index) => (
-              <div
-                key={index}
-                className={`flex gap-2 justify-center py-1 pr-3 pl-1 ${item.model} === model
-                            ? "border-2 border-orange-500 border-solid bg-stone-50"
-                            : "bg-white border-0 rounded-[100px]"
-                            }`}
-              >
-
-                <img loading="lazy" onClick={() => { setModel(item.model) }}
-                  src={item.model === model ? selected : unselected} alt={item.alt} className="shrink-0 w-6 aspect-square" />
-                <div className="flex gap-1 justify-center my-auto">
-                  <img loading="lazy" src={icons[item.name]} alt={`${item.name} icon`} className="shrink-0 aspect-square w-[18px]" />
-                  <div>{item.name}</div>
-                </div>
-              </div>
-            ))}
-        </div>
-      </section>
-      <section className="flex flex-col p-3 mt-6 w-full font-bold bg-gray-100 rounded border-0 border-orange-300 border-solid text-neutral-800 max-md:max-w-full">
-        <div className="flex gap-2 justify-center text-sm bg-gray-100 rounded max-md:flex-wrap">
-          <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/11a0707ac580a78bf668163d79e441fa00442ecabcb2657b7416902ed9404e23?apiKey=aef2252b7f4e44588501764630aaa53c&" alt="Select RAG files icon" className="shrink-0 aspect-[0.97] w-[29px]" />
-          <div className="flex-1 my-auto max-md:max-w-full" onClick={() => {
-            setDisplayMode("rag")
-          }}>Select RAG files (Optional):{" "}</div>
-        </div>
-
-      </section>
     </div>
-  )
-};
+  );
+}
